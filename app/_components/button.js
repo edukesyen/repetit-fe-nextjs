f5bc7a292e82fd22cde2b6347fa322ee8f9d832e
Almaas Izdihar <almaasizdihar@gmail.com>
2024-05-28 22:49:02 +0700

    feat: add dashboard page

---
+++ app/_components/button.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+export function Button({ text, href, variant }) {
+  let btnClassName = "";
+  switch (variant) {
+    case "primary":
+      btnClassName = "bg-light-primary text-white border border-light-primary";
+      break;
+    case "outlined":
+      btnClassName = "text-light-primary border border-light-primary";
+      break;
+    default:
+      break;
+  }
+  return (
+    <Link
+      href={href}
+      className={`w-fit rounded-full text-sm grid place-items-center font-medium py-2.5 px-5 ${btnClassName}`}
+    >
+      {text}
+    </Link>
+  );
+}
---
+++ app/_sections/navbar.js
@@ -0,0 +1,41 @@
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+
+import HomeIcon from "@mui/icons-material/Home";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
+
+import BookIcon from "@mui/icons-material/Book";
+import BookOutlinedIcon from "@mui/icons-material/BookOutlined";
+
+export function Navbar() {
+  return (
+    <nav className="flex-none bg-light-surface-container w-20 min-h-screen flex flex-col items-center justify-between py-4">
+      <div>
+        <NavButton
+          Icon={AccountCircleOutlinedIcon}
+          IconActive={AccountCircleIcon}
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <NavButton Icon={HomeOutlinedIcon} IconActive={HomeIcon} />
+        <NavButton
+          Icon={CalendarMonthOutlinedIcon}
+          IconActive={CalendarMonthIcon}
+        />
+        <NavButton Icon={BookOutlinedIcon} IconActive={BookIcon} />
+      </div>
+      <div></div>
+    </nav>
+  );
+}
+
+function NavButton({ Icon, IconActive }) {
+  return (
+    <button className="w-12 h-12 grid place-items-center rounded-full text-light-primary hover:bg-light-on-surface-variant/[0.08]">
+      <Icon />
+    </button>
+  );
+}
